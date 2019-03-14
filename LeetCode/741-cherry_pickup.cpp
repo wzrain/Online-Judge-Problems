7ebbb0feb24ad4ab52142135efcfd5da36f8ae6b
wzrain <wzrain@163.com>
2019-03-14 23:20:52 +0800

    Update the dp solution of 741 to O(n2) space.

--- LeetCode/741-cherry_pickup.cpp
+++ LeetCode/741-cherry_pickup.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_n3space {
 public:
   int dp[120][55][55];
   int cherryPickup(vector<vector<int>>& grid) {
@@ -31,5 +31,42 @@ public:
       }
     }
     return dp[2 * n - 2][n - 1][n - 1] < 0 ? 0 : dp[2 * n - 2][n - 1][n - 1] ;
+  }
+};
+
+class Solution_n2space {
+public:
+  int dp[55][55];
+  int cherryPickup(vector<vector<int>>& grid) {
+    int n = grid.size();
+    if (n == 0) return 0;
+    for (int j = 0; j < 55; ++j) {
+      for (int k = 0; k < 55; ++k) {
+        dp[j][k] = -1;
+      }
+    }
+    dp[0][0] = 0;
+    for (int s = 0; s < 2 * n - 1; ++s) {
+      for (int i1 = n - 1; i1 >= 0 ; --i1) {
+        for (int i2 = n - 1; i2 >= 0; --i2) {
+          int j1 = s - i1, j2 = s - i2;
+          if (j1 >= n || j2 >= n || j1 < 0 || j2 < 0) continue;
+          if (grid[i1][j1] == -1 || grid[i2][j2] == -1) {
+            dp[i1][i2] = -1;
+            continue;
+          }
+          int tmp = dp[i1][i2];
+          if (i1 > 0 && j2 > 0) tmp = max(tmp, dp[i1 - 1][i2]);
+          if (i2 > 0 && j1 > 0) tmp = max(tmp, dp[i1][i2 - 1]);
+          //if (j2 > 0 && j1 > 0) tmp = max(tmp, dp[i1][i2]);
+          if (i1 > 0 && i2 > 0) tmp = max(tmp, dp[i1 - 1][i2 - 1]);
+          if (tmp < 0) continue;
+          int cur = tmp + grid[i1][j1];
+          if (i1 != i2) cur += grid[i2][j2];
+          dp[i1][i2] = cur;
+        }
+      }
+    }
+    return dp[n - 1][n - 1] < 0 ? 0 : dp[n - 1][n - 1] ;
   }
 };
