6e6fcdb9e9593a45746bf3cffc7cc2e565d4ad99
wzrain <wzrain@163.com>
2019-03-15 23:18:21 +0800

    Add another backtrack solution of 698.

--- LeetCode/698-partition_to_k_equal_sum_subsets.cpp
+++ LeetCode/698-partition_to_k_equal_sum_subsets.cpp
@@ -20,9 +20,37 @@ public:
     for (int i = 0; i < nums.size(); ++i) sum += nums[i];
     if (sum % k) return false;
     int target = sum / k;
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
     vector<int> sums(k, 0);
     return dfs(nums, 0, sums, k, target);
+  }
+};
+
+// quicker backtrack solution(still wondering why)
+// visited indicates the used numbers.
+// still wondering why the pruning from 416 can't be used here.'
+class Solution {
+public:
+  bool dfs(vector<int>& nums, int idx, int k, int target, int sum, vector<int>& visited) {
+    if (k == 0) return true;
+    if (sum == 0) return dfs(nums, 0, k - 1, target, target, visited);
+    for (int i = idx; i < nums.size(); ++i) {
+      if (visited[i]) continue;
+      visited[i] = 1;
+      if(sum >= nums[i] && dfs(nums, i + 1, k, target, sum - nums[i], visited)) 
+        return true;
+      visited[i] = 0;
+    }
+    return false;
+  }
+  bool canPartitionKSubsets(vector<int>& nums, int k) {
+    if (nums.empty()) return true;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); ++i) sum += nums[i];
+    if (sum % k) return false;
+    int target = sum / k;
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+    vector<int> visited(nums.size(), 0);
+    return dfs(nums, 0, k, target, target, visited);
   }
 };
