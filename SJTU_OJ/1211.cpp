85dcbeaa0f4227adbd3b32a9bed7ac8cb5a7f1d8
wzrain <wzrain@163.com>
2019-03-16 15:10:49 +0800

    Add 1211

---
+++ SJTU_OJ/1211.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int n, l[100005], r[1000005], f[100005], root;
+int q[1000005], head = 0, rear = 0;
+
+void init() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; ++i) {
+    scanf("%d%d", &l[i], &r[i]);
+    f[l[i]] = i;
+    f[r[i]] = i;
+  }
+  for (int i = 1; i <= n; ++i) {
+    if (!f[i]) {
+      root = i;
+      break;
+    }
+  }
+  q[rear++] = root;
+}
+
+bool isCBT() {
+  bool reached = false;
+  while (head != rear) {
+    int lc = l[q[head]], rc = r[q[head]];
+    if (!lc && rc) return false;
+    if (reached && lc) return false;
+    if (!rc) reached = true;
+    if (lc) q[rear++] = lc;
+    if (rc) q[rear++] = rc;
+    head++;
+  }
+  return true;
+}
+
+void solve() {
+  if (isCBT()) printf("%c", 'Y');
+  else printf("%c", 'N');
+}
+
+int main() {
+  init();
+  solve();
+  return 0;
+}
