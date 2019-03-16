180d6edff14d46769eb6565b07acc6a9c222dcc5
wzrain <wzrain@163.com>
2019-03-17 00:31:25 +0800

    Add preorder-like solutions of postorder(145).

--- LeetCode/145-binary_tree_postorder_traversal.cpp
+++ LeetCode/145-binary_tree_postorder_traversal.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution_1 {
 public:
   vector<int> postorderTraversal(TreeNode* root) {
     vector<int> res;
@@ -30,6 +30,46 @@ public:
         stk.push_back(tmp);
       }
     }
+    return res;
+  }
+};
+
+// postorder is the reverse of preorder from right to left
+class Solution_2 {
+public:
+  vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    vector<TreeNode*> stk;
+    TreeNode* tmp = root;
+    while (tmp) {
+      res.push_back(tmp->val);
+      if (tmp->left) stk.push_back(tmp->left);
+      tmp = tmp->right;
+      if (!tmp &&!stk.empty()) {
+        tmp = stk.back();
+        stk.pop_back();
+      }
+    }
+    reverse(res.begin(),res.end());
+    return res;
+  }
+};
+
+class Solution_3 {
+public:
+  vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (!root) return res;
+    vector<TreeNode*> stk;
+    stk.push_back(root);
+    while (!stk.empty()) {
+      TreeNode* tmp = stk.back();
+      stk.pop_back();
+      res.push_back(tmp->val);
+      if (tmp->left) stk.push_back(tmp->left);
+      if (tmp->right) stk.push_back(tmp->right);
+    }
+    reverse(res.begin(),res.end());
     return res;
   }
 };
