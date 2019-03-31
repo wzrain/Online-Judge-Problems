d9e748016c5e038ab2f16c811b0c03d59feaf587
wzrain <wzrain@163.com>
2019-03-31 15:59:05 +0800

    Add an iterative scanning solution of 44.

--- LeetCode/044-wildcard_matching.cpp
+++ LeetCode/044-wildcard_matching.cpp
@@ -60,3 +60,52 @@ public:
     return dfs(s, p, 0, 0, visited);
   }
 };
+
+// iterative scanning solution
+// Use two indices to record the position of the last star
+// and the current position of s this * has matched.
+// The key is that if another star appeared at j, that means
+// s[:i] and p[:j] is matched, and if s[i:] and p[j:] doesn't
+// match, the whole string cannot match, which means 
+// the previous star won't help either. 
+// For example if j1 is the first star 's position and j2 is the
+// second, and s[:i2] and p[:j2] is matched. If s[i2:] and p[j2:]
+// cannot match, there's at least s[i3] != p[j3]. So if we want
+// the whole string matches, s[i3] should be matched with star at j1.
+// And if the whole string matches, s[i3 + 1:] and p[j1] matches.
+// However if s[i3 + 1:] and p[j1:] matches, we can just let p[j2]
+// match s[i2:i3] following the same substring that p[j1:j2] matches.
+// Then s[i2:] and p[j2:] matches. Contradiction.
+// So we only need to record the last star's position and 
+// backtrack to that star to add one more character that star matches.
+// In problem 10, this won't work. Counterexample:
+// s: acccccca, p: ac*ccb*a
+class Solution {
+public:
+  bool isMatch(string s, string p) {
+    int i = 0, j = 0, ilast = -1, jlast = -1;
+    while (i < s.length() && j < p.length()) {
+      if (p[j] == '*') {
+        while (j < p.length() && p[j] == '*') j++;
+        ilast = i;
+        jlast = j;
+      }
+      else if (s[i] == p[j] || p[j] == '?') {
+        i++;
+        j++;
+      }
+      else if (ilast > -1){
+        i = ++ilast;
+        j = jlast;
+      }
+      else return false;
+      while (i != s.length() && j == p.length() && ilast > -1) {
+        i = ++ilast;
+        j = jlast;
+      }
+    }
+    if (i != s.length()) return false;
+    while (j < p.length() && p[j] == '*') j++;
+    return j == p.length();
+  }
+};
