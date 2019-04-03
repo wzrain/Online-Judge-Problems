758d3cf1af449002a60f9453b1484f2a722783ad
wzrain <wzrain@163.com>
2019-04-03 12:41:15 +0800

    Add some comments.

--- LeetCode/400-nth_digit.cpp
+++ LeetCode/400-nth_digit.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
   int findNthDigit(int n) {
-    int bd = 0, i = 1, base = 1;
+    int bd = 0, i = 1, base = 1; // If the statement is like pow(base, i) where i increases within the loop,
+                                 // it's good to set a base and multiply with base every loop.
+                                 // pow(k, i) returns double values which are of larger range than int. 
     for (i = 1; i < 9; ++i) {
       if (n > bd + 9 * i * base) {
         bd += 9 * i * base;
