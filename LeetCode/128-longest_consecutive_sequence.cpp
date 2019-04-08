a233b92a3edeca2f9bb658662cc215ba2e93bbae
wzrain <wzrain@163.com>
2019-04-08 10:49:21 +0800

    Add 128

---
+++ LeetCode/128-longest_consecutive_sequence.cpp
@@ -0,0 +1,53 @@
+// When looking for consective subsequences, it's crucial to 
+// check whether a certain element's adjacent elements have appeared.
+// This also contains that sort of "Let the code do the work" thought
+// cuz we don't check the whole sequence but only two adjacent elements.
+class Solution {
+public:
+  int longestConsecutive(vector<int>& nums) {
+    unordered_map<int, int> st;  // st[i] == j => contains a consecutive sequence
+                                 // beginning with i(or j) and ending with j(or i).
+    int res = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (st.find(nums[i]) != st.end()) continue;
+      if (st.find(nums[i] + 1) == st.end() && st.find(nums[i] - 1) == st.end()) {
+        st.insert(make_pair(nums[i], nums[i]));
+        res = max(res, 1);
+      }
+      else if (st.find(nums[i] + 1) != st.end() && st.find(nums[i] - 1) == st.end()) {
+        st.insert(make_pair(nums[i], st[nums[i] + 1]));
+        st[st[nums[i] + 1]] = nums[i];
+        res = max(res, st[nums[i]] - nums[i] + 1);
+      }
+      else if (st.find(nums[i] - 1) != st.end() && st.find(nums[i] + 1) == st.end()) {
+        st.insert(make_pair(nums[i], st[nums[i] - 1]));
+        st[st[nums[i] - 1]] = nums[i];
+        res = max(res,  nums[i] - st[nums[i]] + 1);
+      }
+      else {
+        int e1, e2;
+        e1 = st[nums[i] - 1];
+        e2 = st[nums[i] + 1];
+        st[st[nums[i] + 1]] = e1;
+        st[st[nums[i] - 1]] = e2;
+        res = max(res, e2 - e1 + 1);
+        st.insert(make_pair(nums[i], INT_MAX));
+      }
+      // In fact all we care about is the left and the right
+      // bound of the sequence, so instead of use four ifs, it's
+      // good to just track the left and right, which makes the
+      // code clean and logic clear.
+      // The previous lines could be simplified as follows:
+      /*
+       * int l = nums[i], r = nums[i];
+       * st[nums[i]] = nums[i];
+       * if (st.find(nums[i] + 1) != st.end()) r = st[nums[i] + 1];
+       * if (st.find(nums[i] - 1) != st.end()) l = st[nums[i] - 1];
+       * st[l] = r;
+       * st[r] = l;
+       * res = max(res, r - l + 1);
+       */
+    }
+    return res;
+  }
+};
