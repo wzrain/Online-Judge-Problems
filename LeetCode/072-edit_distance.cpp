967b5ab26d053acd1865343fa1cc4aa3f3c85834
wzrain <wzrain@163.com>
2019-04-09 13:13:21 +0800

    Reduce the space cost of 72 to O(n)

--- LeetCode/072-edit_distance.cpp
+++ LeetCode/072-edit_distance.cpp
@@ -23,3 +23,22 @@ public:
     return dp[l1][l2];
   }
 };
+
+// O(n) space dp solution
+class Solution {
+public:
+  int minDistance(string word1, string word2) {
+    int l1 = word1.length(), l2 = word2.length();
+    vector<vector<int>> dp(2, vector<int>(l2 + 1, 0));
+    for (int j = 0; j <= l2; ++j) dp[0][j] = j;
+    for (int i = 0; i < l1; ++i) {
+      dp[(i + 1) & 1][0] = i + 1;
+      for (int j = 0; j < l2; ++j) {
+        if (word1[i] == word2[j]) dp[(i + 1) & 1][j + 1] = dp[i & 1][j];
+        else dp[(i + 1) & 1][j + 1] = min(dp[i & 1][j + 1], 
+                                          min(dp[(i + 1) & 1][j], dp[i & 1][j])) + 1;
+      }
+    }
+    return dp[l1 & 1][l2];
+  }
+};
