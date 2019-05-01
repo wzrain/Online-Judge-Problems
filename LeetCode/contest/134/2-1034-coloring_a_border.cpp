d34d88fa1fe497b7161cb8ec909868a66ebf5cae
wzrain <wzrain@163.com>
2019-05-01 22:54:30 +0800

    Add a dfs solution of 1034 (Problem 2 of Contest 134).

--- LeetCode/contest/134/2-1034-coloring_a_border.cpp
+++ LeetCode/contest/134/2-1034-coloring_a_border.cpp
@@ -1,3 +1,4 @@
+// bfs solution
 class Solution {
 public:
   vector<vector<int>> colorBorder(vector<vector<int>>& grid, int r0, int c0, int color) {
@@ -30,6 +31,35 @@ public:
         visited.insert(ny + nx * grid[0].size());
       }
     }
+    return grid;
+  }
+};
+
+// dfs solution
+class Solution {
+public:
+  void dfs(vector<vector<int>>& grid, int x, int y, int ic, int color, vector<int>& dir, unordered_set<int>& visited) {
+    for (int d = 0; d < 4; ++d) {
+      int nx = x + dir[d], ny = y + dir[d + 1];
+      if (nx < 0 || nx >= grid.size() || ny < 0 || ny >= grid[0].size()) {
+        grid[x][y] = color;
+        continue;
+      }
+      if(visited.find(ny + nx * grid[0].size()) != visited.end()) continue;
+      if (grid[nx][ny] != ic) {
+        grid[x][y] = color;
+        continue;
+      }
+      visited.insert(ny + nx * grid[0].size());
+      dfs(grid, nx, ny, ic, color, dir, visited);
+    }
+  }
+  vector<vector<int>> colorBorder(vector<vector<int>>& grid, int r0, int c0, int color) {
+    int ic = grid[r0][c0];
+    vector<int> dir = {-1, 0, 1, 0, -1};
+    unordered_set<int> visited;
+    visited.insert(c0 + r0 * grid[0].size());
+    dfs(grid, r0, c0, ic, color, dir, visited);
     return grid;
   }
 };
