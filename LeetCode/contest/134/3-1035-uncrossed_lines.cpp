d2a55793f96f76ae45363b6a8316797c5db1d1c6
wzrain <wzrain@163.com>
2019-05-01 16:45:12 +0800

    Reduce the space cost to O(n) of 1035 (Problem 3 of Contest 134).

--- LeetCode/contest/134/3-1035-uncrossed_lines.cpp
+++ LeetCode/contest/134/3-1035-uncrossed_lines.cpp
@@ -15,3 +15,20 @@ public:
     return dp[A.size()][B.size()];
   }
 };
+
+// O(n) space
+class Solution {
+public:
+  int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+    vector<vector<int>> dp(2, vector<int>(B.size() + 1, 0));
+    for (int i = 0; i < A.size(); ++i) {
+      for (int j = 0; j < B.size(); ++j) {
+        dp[(i + 1) & 1][j + 1] = max(dp[(i + 1) & 1][j], dp[i & 1][j + 1]);
+        if (A[i] == B[j] && dp[(i + 1) & 1][j + 1] < dp[i & 1][j] + 1) {
+          dp[(i + 1) & 1][j + 1] = dp[i & 1][j] + 1;
+        }
+      }
+    }
+    return dp[A.size() & 1][B.size()];
+  }
+};
