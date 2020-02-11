19199c98d6c086cac3942df6b6a35affb192e991
Rain <wzrain@163.com>
2020-02-12 01:39:15 +0800

    Add merge sort solution of 363.

--- LeetCode/363-max_sum_of_rectangle_no_larger_than_k.cpp
+++ LeetCode/363-max_sum_of_rectangle_no_larger_than_k.cpp
@@ -42,3 +42,71 @@ public:
         return res;
     }
 };
+
+
+// Merge sort solution. Idea from 327.
+class Solution {
+public:
+    void Merge(vector<int>& vec, vector<int>& tmp, int l, int mid, int r, int k, int& res) {
+        int pl = l, pr = mid + 1, cur = l;          
+        // int kp = l;     
+        int kp = mid + 1;
+        while (pl <= mid && pr <= r) {
+            if (vec[pl] <= vec[pr]) {
+                while (kp <= r && vec[kp] <= vec[pl] + k) kp++;
+                if (kp != mid + 1) res = max(res, vec[kp - 1] - vec[pl]);
+                tmp[cur++] = vec[pl++];
+            }
+            else {
+                // while (kp <= mid && vec[kp] < vec[pr] - k) kp++;
+                // if (kp <= mid) res = max(res, vec[pr] - vec[kp]);
+                tmp[cur++] = vec[pr++];
+            }
+        }
+        while (pl <= mid) {
+            while (kp <= r && vec[kp] <= vec[pl] + k) kp++;
+            if (kp != mid + 1) res = max(res, vec[kp - 1] - vec[pl]);
+            tmp[cur++] = vec[pl++];
+        }
+        while (pr <= r) {
+            // while (kp <= mid && vec[kp] < vec[pr] - k) kp++;
+            // if (kp <= mid) res = max(res, vec[pr] - vec[kp]);
+            tmp[cur++] = vec[pr++];
+        }
+        for (int i = l; i <= r; ++i) {
+            vec[i] = tmp[i];
+        }
+    }
+    void MergeSort(vector<int>& vec, vector<int>& tmp, int l, int r, int k, int& res) {
+        if (l >= r) return;
+        int mid = (l + r) / 2;
+        MergeSort(vec, tmp, l, mid, k, res);
+        MergeSort(vec, tmp, mid + 1, r, k, res);
+        Merge(vec, tmp, l, mid, r, k, res);
+    }
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        if (matrix.empty()) return 0;
+        int rn = matrix.size(), cn = matrix[0].size();
+        vector<vector<int>> rowsum(rn, vector<int>(cn, 0));
+        for (int i = 0; i < rn; ++i) {
+            rowsum[i][0] = matrix[i][0];
+            for (int j = 1; j < cn; ++j) {
+                rowsum[i][j] = rowsum[i][j - 1] + matrix[i][j];
+            }
+        }
+        int res = INT_MIN;
+        for (int i = 0; i < cn; ++i) {
+            for (int j = i; j < cn; ++j) {
+                vector<int> sum, tmp;
+                sum.push_back(0);
+                for (int r = 0; r < rn; ++r) {
+                    int cur = (rowsum[r][j] - (i == 0 ? 0 : rowsum[r][i - 1]));
+                    sum.push_back(sum.back() + cur);
+                }
+                tmp = sum;
+                MergeSort(sum, tmp, 0, rn, k, res);
+            }
+        }
+        return res;
+    }
+};
