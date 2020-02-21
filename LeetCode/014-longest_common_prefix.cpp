a6400c0c566c4b9661748ee3e2e60e2fd2b7729c
wzrain <wzrain@163.com>
2020-02-22 05:17:37 +0800

    Add Trie solution for 14.

--- LeetCode/014-longest_common_prefix.cpp
+++ LeetCode/014-longest_common_prefix.cpp
@@ -13,3 +13,44 @@ public:
     return res;
   }
 };
+
+// Trie solution. The common prefix stops at latest at isWord.
+class Solution {
+public:
+    struct TrieNode {
+        int idx;
+        bool isWord;
+        TrieNode* next[26];
+        TrieNode() {
+            idx = -1;
+            isWord = false;
+            for (int i = 0; i < 26; ++i) {
+                next[i] = NULL;
+            }
+        }
+    };
+    string longestCommonPrefix(vector<string>& strs) {
+        string res;
+        TrieNode* root = new TrieNode();
+        for (int i = 0; i < strs.size(); ++i) {
+            TrieNode* cur = root;
+            for (char c : strs[i]) {
+                int idx = c - 'a';
+                if (!cur->next[idx]) {
+                    if (cur->idx >= 0 || cur->isWord) break;
+                    cur->idx = idx;
+                    cur->next[idx] = new TrieNode();
+                }
+                cur = cur->next[idx];
+            }
+            cur->isWord = true;
+        }
+        TrieNode* cur = root;
+        while (cur && cur->idx >= 0 && !cur->isWord) {
+            char c = cur->idx + 'a';
+            res += c;
+            cur = cur->next[cur->idx];
+        }
+        return res;
+    }
+};
