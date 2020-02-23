e17ad6f218544787ba185d76a8020223b71c4d50
wzrain <wzrain@163.com>
2020-02-23 17:08:46 +0800

    Add O(n) monotonic queue solution of 862.

--- LeetCode/862-shortest_subarray_with_sum_at_least_k.cpp
+++ LeetCode/862-shortest_subarray_with_sum_at_least_k.cpp
@@ -29,3 +29,38 @@ public:
         return res == INT_MAX ? -1 : res;
     }
 };
+
+// O(n) solution with deque.
+// The element with a larger index and smaller value is always better,
+// because it has a larger chance to be subtracted to get larger than
+// K and yet smaller interval. So we just pop back every element that
+// is smaller than the current one, which leads to a monotonic pattern.
+// One thing is we can do binary search in this monotonic stack and find
+// the nearest element that is smaller than the current one by at least K.
+// However this binary search is unnecessary, because when we get an
+// element that can form a larger-than-K pair with the current one, then
+// every pair it forms with later ones will be longer and unnecessary to
+// consider, which means we can just pop front all the elements that forms
+// such a pair with the current one since they will be useless later.
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        if (A.empty()) return -1;
+        int sz = A.size();
+        vector<int> sum(sz + 1, 0);
+        for (int i = 0; i < sz; ++i) {
+            sum[i + 1] = sum[i] + A[i];
+        }
+        deque<int> dq;
+        int res = INT_MAX;
+        for (int i = 0; i <= sz; ++i) {
+            while (!dq.empty() && sum[i] - sum[dq.front()] >= K) {
+                res = min(res, i - dq.front());
+                dq.pop_front();
+            }
+            while (!dq.empty() && sum[i] < sum[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
