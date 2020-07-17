4c6acd4958816a12fd9c3252e43efbfffbac00d3
wzrain <wzrain@163.com>
2020-07-17 13:35:29 +0800

    Add average O(n) solution of 973.

--- LeetCode/973-k_closest_points_to_origin.cpp
+++ LeetCode/973-k_closest_points_to_origin.cpp
@@ -1,4 +1,4 @@
-// O(nlogn) priority queue solution
+// O(nlogk) priority queue solution
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
@@ -17,5 +17,40 @@ public:
             pq.pop();
         }
         return res;
+    }
+};
+
+// Average O(n) solution based on quicksort
+class Solution {
+public:
+    int division(vector<vector<int>>& pts, int low, int high) {
+        int first = low, last = high;
+        vector<int> key = pts[first];
+        while (first < last) {
+            while (first < last && key[0] * key[0] + key[1] * key[1] <= pts[last][0] * pts[last][0] + pts[last][1] * pts[last][1]) last--;
+            pts[first] = pts[last];
+            while (first < last && key[0] * key[0] + key[1] * key[1] >= pts[first][0] * pts[first][0] + pts[first][1] * pts[first][1]) first++;
+            pts[last] = pts[first];
+        }
+        pts[first] = key;
+        return first;
+    }
+    void kth(vector<vector<int>>& pts, int low, int high, int k) {
+        int key = division(pts, low, high);
+        if (key == k + low - 1) return;
+        if (key > k + low - 1) kth(pts, low, key - 1, k);
+        else kth(pts, key + 1, high, k - key + low - 1);
+        // The implementation could also be:
+        /*
+         * if (low > high) return; // This is unnecessary if we use key == k - 1 rather than key == k
+         * int key = division(pts, low, high);
+         * if (key == k) return;
+         * if (key > k) kth(pts, low, key - 1, k);
+         * else kth(pts, key + 1, high, k);
+         */
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        kth(points, 0, points.size() - 1, K);
+        return vector<vector<int>>(points.begin(), points.begin() + K);
     }
 };
